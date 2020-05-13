b4bf39b49d76137b70887e8b4a722ab4bb89c563
Paresh Rathod <paresh.citrusbug@gmail.com>
2020-05-13 17:09:34 +0530

    file upload

---
+++ AccountSite/myaccounting/src/components/Header.vue
@@ -0,0 +1,60 @@
+<template>
+    <!-- footer section -->
+    <footer>
+        <div class="container">
+            <div class="row">
+
+                <div class="col-md-5 col-sm-4">
+                    <img src="images/logo.png" class="img-responsive" alt="logo">
+                    <p>Onetel is free Bootstrap v3.3.5 HTML5 layout from
+                        <a rel="nofollow" href="http://www.facebook.com/templatemo" target="_parent">templatemo</a> website. Feel free to use it for your website.</p>
+                    <p><i class="fa fa-phone"></i> 010-020-0340</p>
+                    <p><i class="fa fa-envelope-o"></i> info@company.com</p>
+                    <p><i class="fa fa-globe"></i> www.company.com</p>
+                </div>
+
+                <div class="col-md-3 col-sm-4">
+                    <h3>Useful Links</h3>
+                    <p><a href="#">HTML5 Templates</a></p>
+                    <p><a href="#">CSS3 Tricks</a></p>
+                    <p><a href="#">Design Blog</a></p>
+                    <p><a href="#">Animations</a></p>
+                </div>
+
+                <div class="col-md-4 col-sm-4 newsletter">
+                    <h3>Newsletter</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismo.</p>
+                    <div class="input-group">
+                        <form action="#" method="post">
+                            <input name="email" type="email" placeholder="Enter your email" class="form-control" autorequired>
+                            <button type="submit" name="submit" class="btn email">Submit</button>
+                        </form>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </footer>
+    
+    <div class="container">
+        <div class="navbar navbar-default navbar-static-top" role="navigation">
+            <div class="navbar-header">
+                <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon icon-bar"></span>
+                    <span class="icon icon-bar"></span>
+                    <span class="icon icon-bar"></span>
+                </button>
+                <a href="#" class="navbar-brand"><img src="images/logo.png" class="img-responsive" alt="logo"></a>
+            </div>
+            <div class="collapse navbar-collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="index.html" class="active">HOME</a></li>
+                    <li><a href="about.html">ABOUT US</a></li>
+                    <li><a href="portfolio.html">PORTFOLIO</a></li>
+                    <li><a href="contact.html">CONTACT</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+</template>
